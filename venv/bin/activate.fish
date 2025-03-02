0bd6cdc6dff9d6fed048881f251c14f5e91dabd3
depgod <iamshashankpal398@gmail.com>
2025-03-02 11:48:38 +0530

    ~~~

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/user0/private-repos/docs/venv
+set -gx VIRTUAL_ENV /home/user0/private-repos/depgod.github.io/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
